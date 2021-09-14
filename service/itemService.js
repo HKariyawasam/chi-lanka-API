624aba979f15873d9cdfbfd8089505eb53e30ae2
User <hasanigayani@gmail.com>
2021-09-14 18:48:14 +0530

    generated the routes for item service

--- service/itemService.js
+++ service/itemService.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 let Item = require("../class/Item");
 
-router.route("/addItem").post((req,res) => {
+router.route("/addItem").post((req, res) => {
     const itemid = req.body.itemid;
     const itemname = req.body.itemname;
     const price = req.body.price;
@@ -15,10 +15,59 @@ router.route("/addItem").post((req,res) => {
     })
 
     newItem.save().then(() => {
-        res.status(200).send({message: "Item is added"})
+        res.status(200).send({ message: "Item is added" })
     }).catch((err) => {
-        res.status(300).send({ status: "Error with item Insersion", erroe: err.message});
+        res.status(300).send({ status: "Error with item Insersion", erroe: err.message });
     })
+})
+
+//To delete a specific item
+router.route("/removeItem/:itemID").delete(async (req, res) => {
+
+    let item = req.params.itemID;//supplier id taken from frontend
+
+    await Item.findOneAndDelete({ itemid: item })
+        .then(() => {
+            res.status(200).send({ status: "Item deleted" });
+        }).catch(() => {
+            console.log(err);
+            res.status(500).send({ status: "Error with delete of an item", error: err.message });
+        })
+
+})
+
+
+//To display all the items available in system
+router.route("/displayAvailableItems").get((req, res) => {
+
+    Item.find().then((item) => {
+        res.json(item)
+
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+
+//To update a specific item available in system
+router.route("/updateItem/:itemID").put(async (req, res) => {
+
+    let item = req.params.itemID;//supplier Id taken from the frontend
+
+    const { itemid, itemname, price, Description } = req.body;
+
+    const updateItem = {//create a object containing the data that needs to be updated
+        itemid, itemname, price, Description
+    }
+
+    const update = await Item.findOneAndUpdate({ itemid: item }, updateItem)
+        .then(() => {
+            res.status(200).send({ status: "Item Details updated" })//sending details of the updated data back to front end
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send({ status: "Error with updating data of an Item", error: err.message });
+        })
+
 })
 
 module.exports = router;
